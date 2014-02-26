6be50856e117d62f90d6f3aa339443cf4f79e344
Levi Buzolic <levibuzolic@gmail.com>
2014-02-26 17:58:31 +0800

    Tidy up nast code

--- lib/wicked_pdf.rb
+++ lib/wicked_pdf.rb
@@ -55,18 +55,14 @@ class WickedPdf
     if WickedPdf.config[:retreive_version]
       retreive_binary_version
     end
-    puts '===== OPTIONS ====='
-    puts options
-
     temp_path = options.delete(:temp_path)
-    temp_path = "/Users/levi/"
     string_file = WickedPdfTempfile.new("wicked_pdf.html", temp_path)
     string_file.binmode
     string_file.write(string)
     string_file.close
     generated_pdf_file = WickedPdfTempfile.new("wicked_pdf_generated_file.pdf", temp_path)
     generated_pdf_file.binmode
-    command = "\"#{@exe_path}\" #{parse_options(options)} \"file:///#{string_file.path}\" \"#{generated_pdf_file.path}\" " # -q for no errors on stdout
+    command = "\"#{@exe_path}\" #{'-q ' unless on_windows?}#{parse_options(options)} \"file:///#{string_file.path}\" \"#{generated_pdf_file.path}\" " # -q for no errors on stdout
     print_command(command) if in_development_mode?
     err = Open3.popen3(command) do |stdin, stdout, stderr|
       stderr.read
@@ -74,26 +70,17 @@ class WickedPdf
     if return_file = options.delete(:return_file)
       return generated_pdf_file
     end
-    puts "------------------- START PDF GENERATION ----------------"
-    puts string
-    puts return_file
-    # puts pdf
-    puts command
-    puts err
-    puts "------------------- STOP PDF GENERATION ----------------"
     generated_pdf_file.rewind
     f = File.open(generated_pdf_file.path, "r")
     pdf = f.read
-    puts '===== FILE ====='
-    puts pdf
-    puts '===== FILE ====='
-    # raise "PDF could not be generated!\n PDF: #{pdf}\nStrip: #{pdf.rstrip} | #{pdf.rstrip.length}\n Command Error: #{err}" if pdf and pdf.rstrip.length == 0
+    raise "PDF could not be generated!\n Command Error: #{err}" if pdf and pdf.rstrip.length == 0
     pdf
   rescue Exception => e
     raise "Failed to execute:\n#{command}\nError: #{e}"
   ensure
-    string_file.close if string_file
-    generated_pdf_file.close if generated_pdf_file && !return_file
+    f.close
+    string_file.close! if string_file
+    generated_pdf_file.close! if generated_pdf_file && !return_file
   end
 
   private
